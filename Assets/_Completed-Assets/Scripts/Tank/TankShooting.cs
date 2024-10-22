8d6a2ff8d27b750a720daed7e506c97d40b7ec05
Y-Watanabe11 <watanabe42463@gmail.com>
2024-10-22 15:35:25 +0900

    課題1_2砲塔制御
    基本課題の砲塔制御に関する更新を行いました

--- Assets/_Completed-Assets/Scripts/Tank/TankShooting.cs
+++ Assets/_Completed-Assets/Scripts/Tank/TankShooting.cs
@@ -8,6 +8,7 @@ namespace Complete
         public int m_PlayerNumber = 1;              // Used to identify the different players.
         public Rigidbody m_Shell;                   // Prefab of the shell.
         public Transform m_FireTransform;           // A child of the tank where the shells are spawned.
+        public Transform turret;                     // 砲塔のTransform
         public Slider m_AimSlider;                  // A child of the tank that displays the current launch force.
         public AudioSource m_ShootingAudio;         // Reference to the audio source used to play the shooting audio. NB: different to the movement audio source.
         public AudioClip m_ChargingClip;            // Audio that plays when each shot is charging up.
@@ -16,12 +17,27 @@ namespace Complete
         public float m_MaxLaunchForce = 30f;        // The force given to the shell if the fire button is held for the max charge time.
         public float m_MaxChargeTime = 0.75f;       // How long the shell can charge for before it is fired at max force.
 
-
         private string m_FireButton;                // The input axis that is used for launching shells.
         private float m_CurrentLaunchForce;         // The force that will be given to the shell when the fire button is released.
         private float m_ChargeSpeed;                // How fast the launch force increases, based on the max charge time.
         private bool m_Fired;                       // Whether or not the shell has been launched with this button press.
 
+        private void Awake()
+        {
+            // 子オブジェクトから砲塔を探してアサイン
+            if (turret == null)
+            {
+                Transform turretTransform = transform.Find("TankRenderers/TankTurret"); // 子オブジェクトのパスを指定
+                if (turretTransform != null)
+                {
+                    turret = turretTransform;
+                }
+                else
+                {
+                    Debug.LogError("Turret not found. Please assign the turret in the inspector or check the object name.");
+                }
+            }
+        }
 
         private void OnEnable()
         {
@@ -30,8 +46,7 @@ namespace Complete
             m_AimSlider.value = m_MinLaunchForce;
         }
 
-
-        private void Start ()
+        private void Start()
         {
             // The fire axis is based on the player number.
             m_FireButton = "Fire" + m_PlayerNumber;
@@ -40,65 +55,53 @@ namespace Complete
             m_ChargeSpeed = (m_MaxLaunchForce - m_MinLaunchForce) / m_MaxChargeTime;
         }
 
-
-        private void Update ()
+        private void Update()
         {
             // The slider should have a default value of the minimum launch force.
             m_AimSlider.value = m_MinLaunchForce;
 
-            // If the max force has been exceeded and the shell hasn't yet been launched...
             if (m_CurrentLaunchForce >= m_MaxLaunchForce && !m_Fired)
             {
-                // ... use the max force and launch the shell.
                 m_CurrentLaunchForce = m_MaxLaunchForce;
-                Fire ();
+                Fire();
             }
-            // Otherwise, if the fire button has just started being pressed...
-            else if (Input.GetButtonDown (m_FireButton))
+            else if (Input.GetButtonDown(m_FireButton))
             {
-                // ... reset the fired flag and reset the launch force.
                 m_Fired = false;
                 m_CurrentLaunchForce = m_MinLaunchForce;
-
-                // Change the clip to the charging clip and start it playing.
                 m_ShootingAudio.clip = m_ChargingClip;
-                m_ShootingAudio.Play ();
+                m_ShootingAudio.Play();
             }
-            // Otherwise, if the fire button is being held and the shell hasn't been launched yet...
-            else if (Input.GetButton (m_FireButton) && !m_Fired)
+            else if (Input.GetButton(m_FireButton) && !m_Fired)
             {
-                // Increment the launch force and update the slider.
                 m_CurrentLaunchForce += m_ChargeSpeed * Time.deltaTime;
-
                 m_AimSlider.value = m_CurrentLaunchForce;
             }
-            // Otherwise, if the fire button is released and the shell hasn't been launched yet...
-            else if (Input.GetButtonUp (m_FireButton) && !m_Fired)
+            else if (Input.GetButtonUp(m_FireButton) && !m_Fired)
             {
-                // ... launch the shell.
-                Fire ();
+                Fire();
             }
         }
 
+public void Fire()
+{
+    m_Fired = true;
 
-        private void Fire ()
-        {
-            // Set the fired flag so only Fire is only called once.
-            m_Fired = true;
+    // Debugging to check for null references
+    if (m_Shell == null) Debug.LogError("Shell is null");
+    if (m_FireTransform == null) Debug.LogError("FireTransform is null");
+    if (turret == null) Debug.LogError("Turret is null");
 
-            // Create an instance of the shell and store a reference to it's rigidbody.
-            Rigidbody shellInstance =
-                Instantiate (m_Shell, m_FireTransform.position, m_FireTransform.rotation) as Rigidbody;
+    // 砲弾をturretの先端からさらに前方に位置調整
+     Vector3 firePosition = m_FireTransform.position + new Vector3(0, 0.85f, 0); // y方向に少し上に
+    Rigidbody shellInstance = Instantiate(m_Shell, firePosition, turret.rotation) as Rigidbody;
 
-            // Set the shell's velocity to the launch force in the fire position's forward direction.
-            shellInstance.velocity = m_CurrentLaunchForce * m_FireTransform.forward; 
+    shellInstance.velocity = m_CurrentLaunchForce * turret.forward;
 
-            // Change the clip to the firing clip and play it.
-            m_ShootingAudio.clip = m_FireClip;
-            m_ShootingAudio.Play ();
+    m_ShootingAudio.clip = m_FireClip;
+    m_ShootingAudio.Play();
 
-            // Reset the launch force.  This is a precaution in case of missing button events.
-            m_CurrentLaunchForce = m_MinLaunchForce;
-        }
+    m_CurrentLaunchForce = m_MinLaunchForce;
+}
     }
 }
