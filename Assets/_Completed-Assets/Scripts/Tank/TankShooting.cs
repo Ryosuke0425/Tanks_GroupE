963a5190f41c80f1e78a6d1eb710dd1c9278c5f7
Hisayuki Mutsuda <hk000sy545au-i@outlook.jp>
2024-11-14 08:47:23 +0900

    1_3の修正とユーザ管理課題9割
    ユーザ被りの許容をできるように修正したい
    ユーザ名の初期値が一律NoNameになっていない

--- Assets/_Completed-Assets/Scripts/Tank/TankShooting.cs
+++ Assets/_Completed-Assets/Scripts/Tank/TankShooting.cs
@@ -97,21 +97,10 @@ namespace Complete
             m_AimSlider.transform.rotation = turret.rotation * Quaternion.Euler(90, 0, 0);  // 砲塔の回転に合わせて矢印を表示
 
             // The slider should have a default value of the minimum launch force.
-            m_AimSlider.value = m_MinLaunchForce;
+            //m_AimSlider.value = m_MinLaunchForce;
             if (m_Bullets_hold > 0)
             {
-                if (m_CurrentLaunchForce >= m_MaxLaunchForce && !m_Fired)
-                {
-                    m_CurrentLaunchForce = m_MaxLaunchForce;//力が最大になった放出
-                    //Fire();
-                    isIncreasing = false;
-                }
-                else if (m_CurrentLaunchForce < m_MinLaunchForce && !m_Fired)
-                {
-                    m_CurrentLaunchForce = m_MinLaunchForce;
-                    isIncreasing = true;
-                }
-                else if (Input.GetButtonDown(m_FireButton))
+                if (Input.GetButtonDown(m_FireButton))
                 {
                     m_Fired = false;
                     m_CurrentLaunchForce = m_MinLaunchForce;//ボタンが押された時は最小値を与える
@@ -124,29 +113,27 @@ namespace Complete
                     if (isIncreasing == true)
                     {
                         m_CurrentLaunchForce += m_ChargeSpeed * Time.deltaTime;//押されている最中は力も大きくなる
-                        m_AimSlider.value = m_CurrentLaunchForce;
                         if (m_CurrentLaunchForce >= m_MaxLaunchForce)
                         {
-                            m_CurrentLaunchForce = m_MinLaunchForce;
+                            m_CurrentLaunchForce = m_MaxLaunchForce;
                             isIncreasing = false;
                         }
                     }
                     else if (isIncreasing == false)
                     {
                         m_CurrentLaunchForce -= m_ChargeSpeed * Time.deltaTime;
-                        m_AimSlider.value = m_CurrentLaunchForce;
                         if (m_CurrentLaunchForce <= m_MinLaunchForce)
                         {
                             m_CurrentLaunchForce = m_MinLaunchForce;
                             isIncreasing = true;
                         }
                     }
-                    //m_AimSlider.value = m_CurrentLaunchForce;
+                    m_AimSlider.value = m_CurrentLaunchForce;
                 }
                 else if (Input.GetButtonUp(m_FireButton) && !m_Fired)
                 {
                     Fire();
-                    
+                    m_AimSlider.value = m_MinLaunchForce;
                 }
             }
 
--- Assets/_Completed-Assets/Scripts/UI/StartButton.cs
+++ Assets/_Completed-Assets/Scripts/UI/StartButton.cs
@@ -12,6 +12,14 @@ public class CreateUserData
 {
     public string type;
     public string username;
+    public string user_id;
+}
+[System.Serializable]
+public class ResponseData
+{
+    public string status;
+    public string user_id;
+    public string error;
 }
 public class StartButton : MonoBehaviour
 {
@@ -19,7 +27,7 @@ public class StartButton : MonoBehaviour
     private string userId;
     private string user_Name = "NoName";
     [SerializeField] private TextMeshProUGUI user_ID;//左上のTextMeshProに表示したい内容を保持するため
-    [SerializeField] private TMP_InputField usernameInputField;//ユーザ名を入力を保持するための
+    //[SerializeField] private TMP_InputField usernameInputField;//ユーザ名を入力を保持するための
     [SerializeField] private Button Button;
     private bool isMessageReceived = false;
 
@@ -38,12 +46,16 @@ public class StartButton : MonoBehaviour
         if (PlayerPrefs.HasKey("UserID"))//データをローカルに保存するためのクラス、UserIDがあるときは以下の処理
         {
             userId = PlayerPrefs.GetString("UserID");
+            Debug.Log("already Login:" + userId);
             DisplayUserId(userId); // タイトル画面左上に表示
         }
-        Debug.Log("OK" + userId);
+        else
+        {
+            Debug.Log("first Login");
+        }
         //this.usernameInputField = GameObject.Find("UsernameInputField").GetComponent<TMP_InputField>();//ユーザの入力を可能にする
         Button.onClick.AddListener(OnTapToStart);
-        usernameInputField.onEndEdit.AddListener(OnInputEnd);
+        //usernameInputField.onEndEdit.AddListener(OnInputEnd);
     }
 
     public void OnTapToStart()//startbuttonが押されたとき、startbuttonに呼び出す処理を追加予定
@@ -60,18 +72,20 @@ public class StartButton : MonoBehaviour
 
     private void CreateNewUser()//データを送る操作
     {
+        user_Name = "Player" + Random.Range(1, 10000);
         //string username = string.IsNullOrEmpty(usernameInputField.text) ? "Player" + Random.Range(1, 10000) : usernameInputField.text;//下のusernameに入れる
-        var createUserData = new CreateUserData{ type = "create_user", username = user_Name };//typeを指定してあげる事でサーバでの処理の分岐に役立つ
-        Debug.Log(createUserData);
+        var createUserData = new CreateUserData{ type = "create_user", username = user_Name };//typeを指定してあげる事でサーバでの処理の分岐に役立つ、CreateUserDataクラスの属性すべてが送信される
+        Debug.Log("success create user:"+createUserData);
         string JsonMessage = JsonUtility.ToJson(createUserData);
-        Debug.Log(JsonMessage);
-        ws.Send(JsonMessage);//ws.SendJsonUtility.ToJson(Data)でデータを送る
+        Debug.Log("success convert userdata to Json:" + JsonMessage);
+        ws.Send(JsonMessage);//ws.SendJson(Utility.ToJson(Data))でデータを送る
     }
 
     private void LoginUser()//データを送る操作
     {
+        user_Name = PlayerPrefs.GetString("UserName");
         var loginData = new CreateUserData{ type = "login", username = user_Name };
-        Debug.Log(loginData);
+        Debug.Log("success login:" + loginData);
         ws.Send(JsonUtility.ToJson(loginData));//データを送る
     }
 
@@ -79,11 +93,11 @@ public class StartButton : MonoBehaviour
     {
         Debug.Log(e.Data);
         var response = JsonUtility.FromJson<ResponseData>(e.Data);//responseはstatusとuser_idを持つ
-        Debug.Log(response.status);
-        Debug.Log(response.user_id);
+        Debug.Log("In OnMessageReceived function display status:" + response.status);
+        Debug.Log("In OnMessageReceived function display user_id:" + response.user_id);
         if (response.status == "success" && !string.IsNullOrEmpty(response.user_id))
         {
-            Debug.Log("sucess OK");
+            Debug.Log("sucess receive message from server");
             userId = response.user_id;//メインスレッドでローカルでのデータ保持を行うため一時保存
             isMessageReceived = true;//メインスレッドで処理を行うため
             Debug.Log("tempSave OK");
@@ -107,7 +121,7 @@ public class StartButton : MonoBehaviour
         if (user_ID != null)
         {
             // タイトル画面のUIにユーザーIDを表示する処理を追加
-            Debug.Log("UserID: " + userId);
+            Debug.Log("OK DisplayUserID: " + userId);
             this.user_ID.text = "UserID:" + userId;//userIdを画面上のTextMeshProに表示、うまくできてない
         }
         else
@@ -119,7 +133,7 @@ public class StartButton : MonoBehaviour
     {
         Debug.Log("OK preScenetrans");
         // 必要な処理をここに挿入 (例えば、待機時間やアニメーション)
-        yield return new WaitForSeconds(2); // 例えば1秒待機
+        yield return new WaitForSeconds(1); // 例えば1秒待機
         SceneManager.LoadScene(SceneNames.HomeScene); // 次のシーンへ遷移
     }
 
@@ -130,17 +144,18 @@ public class StartButton : MonoBehaviour
             ws.Close();
         }
     }
-
+    /*//ユーザー名をtitle画面でも入力する時に使用
     private void OnInputEnd(string username)//ユーザ名に関する仕様を考慮
     {
+        Debug.Log("In OnInputEnd function:" + username);
         if (3 < username.Length && username.Length < 15) //&& ContainsInvalidCharacters(username))
         {
             user_Name = username;
-            Debug.Log(user_Name);
+            Debug.Log("In OnInputEnd function username OK:" + user_Name);
         }
         else
         {
-            Debug.Log("3文字以上15文字以下、特殊記号なしで入力してください");
+            Debug.Log("In OnInputEnd function username No:3文字以上15文字以下、特殊記号なしで入力してください");
         }
     }
     bool ContainsInvalidCharacters(string input)
@@ -148,14 +163,8 @@ public class StartButton : MonoBehaviour
         // 記号を含む場合は true を返す（アルファベット、数字、ひらがな、カタカナ、漢字以外）
         return Regex.IsMatch(input, @"[^a-zA-Z0-9ぁ-んァ-ン一-龯]");
     }
-
-    [System.Serializable]
-    private class ResponseData
-    {
-        public string status;
-        public string user_id;
-        public string error;
-    }
+    */
+    
     void Update()
     {
         if (isMessageReceived) //以下はメインスレッドで処理しないといけないのでここ
@@ -163,9 +172,8 @@ public class StartButton : MonoBehaviour
             PlayerPrefs.SetString("UserID", userId.ToString());//ローカルにデータを保存、スコア何かも保存できる、PlayerPrefs.DeleteKey("UserID");によりデータの削除もできる
             PlayerPrefs.SetString("UserName", user_Name);
             PlayerPrefs.Save();//うまくできてない,WebSocketメッセージを受信している際に使えない
-            Debug.Log(PlayerPrefs.HasKey("UserID"));
-            Debug.Log(PlayerPrefs.GetString("UserID"));
-            Debug.Log(PlayerPrefs.GetString("UserName"));
+            Debug.Log("display resister UserID:" + PlayerPrefs.GetString("UserID"));
+            Debug.Log("display resister UserName:" + PlayerPrefs.GetString("UserName"));
             DisplayUserId(userId); // タイトル画面左上に表示
             StartCoroutine(TransitionToNextScene());
             isMessageReceived = false;//一度のみ呼び出せる
