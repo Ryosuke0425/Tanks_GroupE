f04ffc9e07e5dc63733a89d97308bbf8d40ce8bd
Y-Watanabe11 <watanabe42463@gmail.com>
2024-12-10 13:00:09 +0900

    lobby setting

--- Assets/_Completed-Assets/Scripts/Definitions/SceneNames.cs
+++ Assets/_Completed-Assets/Scripts/Definitions/SceneNames.cs
@@ -8,4 +8,5 @@ public static class SceneNames
     public static readonly string HomeScene = "HomeScene";
     public static readonly string CompleteGameScene = "_Complete-Game";
     public static readonly string UserRegistrationScene = "UserRegistrationScene";
+    public static readonly string LobbyScene = "LobbyScene";
 }
